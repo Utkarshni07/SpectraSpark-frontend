02f00d0df0283347d02e1e023b29ca6a2fd0fd24
Utkarshni <theutkarshni@gmail.com>
2025-05-11 00:06:04 +0530

    updated header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 function Header() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMobile(!isMobile);
+  };
+
+  const closeMenu = () => {
+    setIsMobile(false);
+  };
+
   return (
     <header className="header">
       <div className="logo">SpectraSpark<span> Technology</span></div>
-      <nav>
-        <ul className="nav-links">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/services">Services</Link></li>
-          <li><Link to="/strengths">Strengths</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-        </ul>
+      
+      <div className="menu-icon" onClick={toggleMenu}>
+        {isMobile ? <FaTimes /> : <FaBars />}
+      </div>
+
+      <nav className={isMobile ? "nav-links active" : "nav-links"}>
+        <li><Link to="/" onClick={closeMenu}>Home</Link></li>
+        <li><Link to="/services" onClick={closeMenu}>Services</Link></li>
+        <li><Link to="/strengths" onClick={closeMenu}>Strengths</Link></li>
+        <li><Link to="/contact" onClick={closeMenu}>Contact</Link></li>
       </nav>
     </header>
   );
